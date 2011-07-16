9404d7982b997631ff9c8bb08d8a34e6a60e0883
Charles Chen <ccchen920@gmail.com>
2011-07-16 13:44:47 -0700

    Created User model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  attr_accessor :name, :email
+  
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
