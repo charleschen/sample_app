c54ecc07f0cd35480c4f7606d7853968c292dc20
Charles Chen <ccchen920@gmail.com>
2011-07-17 20:26:29 -0700

    added password and encrypted_password attributes to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password # virtual attribute that's created
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\-\d.]+\.[a-z]+\z/i
   
@@ -11,6 +12,10 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex},
                     :uniqueness => { :case_sensitive => false}
+  
+  validates :password,  :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
 end
 # == Schema Information
 #
