6cfceefe627c4f57e1a4bde1bd0d85e1b8c56e5c
Charles Chen <ccchen920@gmail.com>
2011-07-18 01:09:05 -0700

    Added a User factory and tests for show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Charles Chen"
+  user.email                  "Charles@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
